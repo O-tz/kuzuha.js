a197f049b1612ae391509697a02aa75559bebd56
O-tz <112265277+O-tz@users.noreply.github.com>
2023-03-12 23:49:44 +0900

    コメントを編集した

--- routes/bbs.js
+++ routes/bbs.js
@@ -1,32 +1,16 @@
 let express = require("express");
 let router = express.Router();
 
-// コントローラーを要求
+// コントローラーのメソッドを利用可能にする
 let bbsController = require("../controllers/bbsController");
 
 // BBS 関連ルート //
 
-// GET BBS //
 router.get("/read", bbsController.bbs);
 router.get("/", bbsController.bbs);
-/*
-// GET BBS with query //
-// next page
-router.get("/:mattaritime.:since-", bbsController.bbsIntegration);
-// mattariload
-router.get("/-:until", bbsController.bbsIntegration);
-//次のページ・まったりロード統合
-*/
-//router.get("/:mattaritime.:since-:until", bbsController.bbsIntegration);
-
-// POST メッセージ //
 router.post("/", bbsController.bbsMessagePost);
-
-// GET フォロー投稿//
 router.get("/follow/:id", bbsController.bbsFollowPostPage); //直接のフォロー先ID
-// POST フォロー投稿//
 router.post("/follow/:targetid", bbsController.bbsFollowPost); //根のフォロー先ID
-
 // GET スレッド表示//
 router.get("/thread/:id", bbsController.bbsThreadShow);
 
